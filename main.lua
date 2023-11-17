da92e7a420da3b44423befb366181da61e171da9
iamcheeseman <xanderswan32@gmail.com>
2023-11-16 19:52:33 -0500

    Temp object spawning

--- main.lua
+++ main.lua
@@ -12,12 +12,22 @@ local Player = require("player")
 local layers = {}
 
 function love.load()
-  core.objs:add(Player())
   layers = core.tiled.openLevel(assetDirectory, "assets.level1")
 
   for _, v in ipairs(layers) do
     if v.type == "tilelayer" then
       core.tiled.generateTileLayerCollision(v)
+    end
+
+    if v.type == "objectgroup" then
+      for _, obj in ipairs(v.objs) do
+        if obj.type == "Player" then
+          local player = Player()
+          player.x = obj.x
+          player.y = obj.y
+          core.objs:add(player)
+        end
+      end
     end
   end
 end
