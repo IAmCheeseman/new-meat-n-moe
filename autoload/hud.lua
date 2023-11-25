6eefb5a1c013c66f0e793b0dea2dfa899c89c55a
iamcheeseman <xanderswan32@gmail.com>
2023-11-25 10:55:16 -0500

    better healthbars

--- autoload/hud.lua
+++ autoload/hud.lua
@@ -3,7 +3,7 @@ local characters = require("require.characters")
 local font = love.graphics.newImageFont("assets/fonts/custom.png", " abcdefghijklmnopqrstuvwxyz0123456789.,;!'\"/\\")
 
 
-local function drawCharacterHealth(x, y, w, h, r, g, b, tr, tg, tb, character)
+local function drawCharacterHealth(x, y, w, h, r, g, b, ta, character)
   local health = character.health
   local maxHealth = character.maxHealth
 
@@ -16,7 +16,7 @@ local function drawCharacterHealth(x, y, w, h, r, g, b, tr, tg, tb, character)
   love.graphics.rectangle("fill", x + 1, y + 1, progressWidth, h - 2)
 
   love.graphics.setFont(font)
-  love.graphics.setColor(tr, tg, tb)
+  love.graphics.setColor(1, 1, 1, ta)
   love.graphics.print(("%d/%d"):format(health, maxHealth), x + w + 2, y)
 
   love.graphics.setColor(1, 1, 1)
@@ -26,6 +26,6 @@ core.event.connect("gui", function()
   local active = characters.getActive()
   local inactive = characters.getInactive()
 
-  drawCharacterHealth(1, 1, 48, 8, 1, 0, 0, 1, 1, 1, active)
-  drawCharacterHealth(1, 10, 48, 8, 0.5, 0, 0, 0.8, 0.8, 0.8, inactive)
+  drawCharacterHealth(1, 1, 48, 8, 1, 0, 0, 1, active)
+  drawCharacterHealth(1, 10, 48, 4, 0.5, 0, 0, 0, inactive)
 end)
